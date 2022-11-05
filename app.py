a1bd50d6086806f5a92132f791c6d339b61d6373
osmanhaji <ohaj0001@student.monash.edu>
2022-11-05 21:24:42 +1100

    added functionality to save tweets to database

--- app.py
+++ app.py
@@ -9,17 +9,13 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'twitter'
 
-
-cnx = mysql.connector.connect(user = 'root', database = 'twitter')
-
-cursor = cnx.cursor()
-
-#cursor.execute(''' insert into users values (1, 'leron', 'james', 'ohio') ''')
-
-
-cnx.commit()
-
-cursor.close()
+def submit_query(query):
+    # helper function to establish connection and submit queries
+    cnx = mysql.connector.connect(user = 'root', database = 'twitter')
+    cursor = cnx.cursor()
+    cursor.execute(query)
+    cnx.commit()
+    cursor.close()
 
 @app.route("/")
 def index():
@@ -35,7 +31,11 @@ def login():
         return "Login via the login Form"
     if request.method == 'POST':
         tweet = request.form['tweet']
-        return tweet
+        ip = request.environ['REMOTE_ADDR']
+        query = f'''insert into tweet(ip, post) values ('{ip}', '{tweet}')'''
+        submit_query(query)
+        return render_template("form.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
