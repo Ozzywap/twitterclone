ef9fba4878cb86ed9dfa92550fb1f83d267f0470
osmanhaji <ohaj0001@student.monash.edu>
2022-11-05 17:40:49 +1100

    fixed connection issues and can add data into db now

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -7,18 +8,22 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'twitter'
- 
-mysql = MySQL(app)
 
 
-cursor = mysql.connection.cursor()
+cnx = mysql.connector.connect(user = 'root', database = 'twitter')
+
+cursor = cnx.cursor()
 
 cursor.execute(''' insert into users values (1, 'leron', 'james', 'ohio') ''')
 
-mysql.connection.commit()
+
+cnx.commit()
 
 cursor.close()
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
