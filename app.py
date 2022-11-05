7e65e8f180d2fd24aabd22adccb213da4698360e
osmanhaji <ohaj0001@student.monash.edu>
2022-11-05 15:00:42 +1100

    establishing connection to mysql to store data - bug with connecting

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import Flask, render_template, request
-
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'twitter'
+ 
+mysql = MySQL(app)
+
+
+cursor = mysql.connection.cursor()
+
+cursor.execute(''' insert into users values (1, 'leron', 'james', 'ohio') ''')
+
+mysql.connection.commit()
+
+cursor.close()
 
 @app.route("/")
 def index():
